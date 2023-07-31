0b95f3cef07a5857e4c15a8866c18f66bf0d0930
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-07-31 10:00:54 -0400

    added comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,28 @@
+// Declares variables using ID tags from HTML
 let quizDiv = document.querySelector("#quiz")
 let questionDisplay = document.querySelector("#question")
 let button1 = document.querySelector("#answer1")
 let button2 = document.querySelector("#answer2")
 let button3 = document.querySelector("#answer3")
 let button4 = document.querySelector("#answer4")
+let countdown = document.querySelector("#timer")
 
+// Sets timer variable
 let timer = 90
 
+// Interval to reduce timer by 1 for every 1000ms
 let timerId = setInterval(function(){
 
     timer -=1
-    console.log(timer)
-    
+     
 }, 1000)
 
-let questions = [{question: "Example question goes here", answers: ["one", "two", "three", "four"], correctAnswer: "four"},
+// declares questions as a set of arrays
+let questions = [{question: "The 'funtion' and 'var' are known as:", answers: ["Keywords", "Data Types", "Declaration Statements", "Prototypes"], correctAnswer: "Declaration Statements"},
 {question: "Example question goes here", answers: ["one", "two", "three", "four"], correctAnswer: "three"}
 ]
 
+// declares currentQuestion as 0, used to cycle through array
 let currentQuestion = 0
 
 renderQuestion();
@@ -29,9 +34,10 @@ function renderQuestion(){
     button2.textContent = (questions[currentQuestion].answers[1])
     button3.textContent = (questions[currentQuestion].answers[2])
     button4.textContent = (questions[currentQuestion].answers[3])
-    // = (questions[currentQuestion].correctAnswer)
+     = (questions[currentQuestion].correctAnswer)
 }   
 
+// Event listening for a click inside of quizDiv
 quizDiv.addEventListener("click", function(event){
 
     if(event.target.matches("button")){
