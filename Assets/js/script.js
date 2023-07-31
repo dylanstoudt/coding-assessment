c997a52783462511f1f01d2e9e44f3133298dc2a
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-07-31 10:00:29 -0400

    Updated renderquestion() and added an event listener in JS, Also hooked into buttons, h1, and and main div inside of body

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,9 @@
-
-
+let quizDiv = document.querySelector("#quiz")
+let questionDisplay = document.querySelector("#question")
+let button1 = document.querySelector("#answer1")
+let button2 = document.querySelector("#answer2")
+let button3 = document.querySelector("#answer3")
+let button4 = document.querySelector("#answer4")
 
 let timer = 90
 
@@ -16,12 +20,22 @@ let questions = [{question: "Example question goes here", answers: ["one", "two"
 
 let currentQuestion = 0
 
+renderQuestion();
+
 function renderQuestion(){
 
-    console.log(questions[currentQuestion].question)
-    console.log(questions[currentQuestion].answers[0])
-    console.log(questions[currentQuestion].answers[1])
-    console.log(questions[currentQuestion].answers[2])
-    console.log(questions[currentQuestion].answers[3])
-    console.log(questions[currentQuestion].correctAnswer)
+    questionDisplay.textContent = (questions[currentQuestion].question)
+    button1.textContent = (questions[currentQuestion].answers[0])
+    button2.textContent = (questions[currentQuestion].answers[1])
+    button3.textContent = (questions[currentQuestion].answers[2])
+    button4.textContent = (questions[currentQuestion].answers[3])
+    // = (questions[currentQuestion].correctAnswer)
 }   
+
+quizDiv.addEventListener("click", function(event){
+
+    if(event.target.matches("button")){
+        currentQuestion++
+        renderQuestion()
+    }
+})
